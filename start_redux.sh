fa175d32a34da57954af32171f8f235b1e7f5f5e
Carlos Loureda <balanzeneto@gmail.com>
2019-05-10 07:14:11 +0200

    Added reducers and middleware

--- start_redux.sh
+++ start_redux.sh
@@ -3,6 +3,9 @@ touch ./src/actions/index.js
 mkdir ./src/reducers
 touch ./src/reducers/index.js
 mkdir ./src/store
-touch ./src/store/index.js
+#touch ./src/store/index.js
 mkdir ./src/middleware
 touch ./src/middleware/index.js
+
+yarn add react-redux redux redux-thunk
+yarn add -D redux-devtools-extension
