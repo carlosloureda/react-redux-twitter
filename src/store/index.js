9e2eaf6f94636988219a7558b5783c19ffd15890
Carlos Loureda <balanzeneto@gmail.com>
2019-05-10 07:28:55 +0200

    Init app data and add redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "../reducers";
 import middleware from "../middleware";
 
-const store = createStore(reducer, middleware);
+const store = createStore(reducer, composeWithDevTools(middleware));
 export default store;
